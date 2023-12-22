4943d28a72cd62d93049bb10542345559562a4d8
Jane49-cloud <janendirangu49@gmail.com>
2023-12-22 12:31:59 +0300

    debug

--- src/constants/loader.tsx
+++ src/constants/loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
     return (
-        <div className="flex bg-black p-5 justify-center  items-center  fixed inset-1 h-full bg-opacity-20 z-40">
+        <div className="">
             <div className="loader animate-bounce"></div>
         </div>
     );
