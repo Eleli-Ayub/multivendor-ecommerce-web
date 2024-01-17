7343bf7a4a47cb291b06b5788fc5a38cd43efac2
Jane49-cloud <janendirangu49@gmail.com>
2024-01-17 18:56:55 +0300

    debug

--- src/Redux/hooks/inquiry.tsx
+++ src/Redux/hooks/inquiry.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'http://localhost:5000/inquiries';
+const url = 'https://inquiries.vercel.app/inquiries';
 
 export const createInquiry = async (payload: any): Promise<any> => {
     try {
