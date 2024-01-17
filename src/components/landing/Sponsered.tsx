0c0d0c86bca58b96142f240a806963d3d23209ed
Jane49-cloud <janendirangu49@gmail.com>
2024-01-17 05:27:52 +0300

    debug landing design

--- src/components/landing/Sponsered.tsx
+++ src/components/landing/Sponsered.tsx
@@ -22,7 +22,7 @@ const Popular = ({ Ads }: any) => {
 
   return (
     <div className="px-4  w-full overflow-hidden bg-white shadow-lg">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 w-full py-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 w-full py-4">
         {Ads?.slice(0, 6).map((product: any) => (
           <SponsereCard
             key={product.product_data?.producttid}
