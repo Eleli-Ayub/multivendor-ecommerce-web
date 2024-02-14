2346602f635193489e6159122e5b1bdba977a087
Jane49-cloud <janendirangu49@gmail.com>
2024-02-14 12:07:08 +0300

    draft locations structure

--- src/data/Location.tsx
+++ src/data/Location.tsx
@@ -1,66 +1,266 @@
 export const locations = [
-    { id: 56, name: 'Embakasi' },
-    { id: 52, name: 'Eastleigh' },
-    { id: 62, name: 'Donholm' },
-    { id: 48, name: 'Karen' },
-    { id: 43, name: 'Kajiado' },
-    { id: 3, name: 'Kakamega' },
-    { id: 10, name: 'Kakamega' },
-    { id: 32, name: 'Kakamega' },
-    { id: 45, name: 'Kericho' },
-    { id: 63, name: 'Kasarani' },
-    { id: 50, name: 'Kileleshwa' },
-    { id: 60, name: 'Kilifi' },
-    { id: 37, name: 'Kisii' },
-    { id: 37, name: 'Kisumu' },
-    { id: 65, name: 'Nairobi City County' },
-    { id: 42, name: 'Nandi' },
-    { id: 39, name: 'Migori' },
-    { id: 61, name: 'Meru' },
-    { id: 16, name: 'Mandera' },
-    { id: 44, name: 'Marsabit' },
-    { id: 21, name: "Murang'a" },
-    { id: 18, name: 'Nyandarua' },
-    { id: 40, name: 'Nyamira' },
-    { id: 19, name: 'Nyamira' },
-    { id: 54, name: 'Ngong' },
-    { id: 41, name: 'Tharaka-Nithi' },
-    { id: 34, name: 'Bungoma' },
-    { id: 35, name: 'Busia' },
-    { id: 36, name: 'Siaya' },
-    { id: 38, name: 'Homa Bay' },
-    { id: 9, name: 'Mombasa' },
-    { id: 46, name: 'Laikipia' },
-    { id: 23, name: 'Turkana' },
-    { id: 26, name: 'Trans-Nzoia' },
-    { id: 27, name: 'Uasin Gishu' },
-    { id: 28, name: 'Elgeyo-Marakwet' },
-    { id: 29, name: 'Nandi' },
-    { id: 30, name: 'Bomet' },
-    { id: 31, name: 'Kericho' },
-    { id: 33, name: 'Vihiga' },
-    { id: 11, name: 'Isiolo' },
-    { id: 34, name: 'Bungoma' },
-    { id: 35, name: 'Busia' },
-    { id: 36, name: 'Siaya' },
-    { id: 37, name: 'Kisumu' },
-    { id: 38, name: 'Homa Bay' },
-    { id: 39, name: 'Migori' },
-    { id: 40, name: 'Kisii' },
-    { id: 41, name: 'Nyamira' },
-    { id: 43, name: 'Kajiado' },
-    { id: 45, name: 'Kericho' },
-    { id: 47, name: 'Nakuru' },
-    { id: 48, name: 'Lavington' },
-    { id: 49, name: 'Lavington' },
-    { id: 51, name: 'Parklands' },
-    { id: 53, name: "Lang'ata" },
-    { id: 55, name: 'Ruaka' },
-    { id: 57, name: 'South C' },
-    { id: 58, name: 'South B' },
-    { id: 59, name: 'Runda' },
-    { id: 60, name: 'Gigiri' },
-    { id: 61, name: 'Muthaiga' },
-    { id: 63, name: 'Kasarani' },
-    { id: 64, name: 'Muthaiga' },
+    {
+        id: 1,
+        name: 'Mombasa',
+        subLocations: [
+            {
+                id: 1,
+                name: 'Bamburi',
+            },
+            {
+                id: 2,
+                name: 'changamwe',
+            },
+            {
+                id: 3,
+                name: 'mtwapa',
+            },
+            {
+                id: 4,
+                name: 'nyali',
+            },
+            {
+                id: 5,
+                name: 'likoni',
+            },
+            {
+                id: 6,
+                name: 'Kongowea',
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: 'Kwale',
+    },
+    {
+        id: 3,
+        name: 'Kilifi',
+    },
+    {
+        id: 4,
+        name: 'Tana River',
+    },
+    {
+        id: 5,
+        name: 'Lamu',
+    },
+    {
+        id: 6,
+        name: 'Taita–Taveta',
+    },
+    {
+        id: 7,
+        name: 'Garissa',
+    },
+    {
+        id: 8,
+        name: 'Wajir',
+    },
+    {
+        id: 9,
+        name: 'Mandera',
+    },
+    {
+        id: 10,
+        name: 'Marsabit',
+    },
+    {
+        id: 11,
+        name: 'Isiolo',
+    },
+    {
+        id: 12,
+        name: 'Meru',
+    },
+    {
+        id: 13,
+        name: 'Tharaka-Nithi',
+    },
+    {
+        id: 14,
+        name: 'Embu',
+    },
+    {
+        id: 15,
+        name: 'Kitui',
+    },
+    {
+        id: 16,
+        name: 'Machakos',
+    },
+    {
+        id: 17,
+        name: 'Makueni',
+    },
+    {
+        id: 18,
+        name: 'Nyandarua',
+    },
+    {
+        id: 19,
+        name: 'Nyeri',
+    },
+    {
+        id: 20,
+        name: 'Kirinyaga',
+    },
+    {
+        id: 21,
+        name: "Murang'a",
+    },
+    {
+        id: 22,
+        name: 'Kiambu',
+    },
+    {
+        id: 23,
+        name: 'Turkana',
+    },
+    {
+        id: 24,
+        name: 'West Pokot',
+    },
+    {
+        id: 25,
+        name: 'Samburu',
+    },
+    {
+        id: 26,
+        name: 'Trans-Nzoia',
+    },
+    {
+        id: 27,
+        name: 'Uasin Gishu',
+    },
+    {
+        id: 28,
+        name: 'Elgeyo-Marakwet',
+    },
+    {
+        id: 29,
+        name: 'Nandi',
+    },
+    {
+        id: 30,
+        name: 'Baringo',
+    },
+    {
+        id: 31,
+        name: 'Laikipia',
+    },
+    {
+        id: 32,
+        name: 'Nakuru',
+    },
+    {
+        id: 33,
+        name: 'Narok',
+    },
+    {
+        id: 34,
+        name: 'Kajiado',
+    },
+    {
+        id: 35,
+        name: 'Kericho',
+    },
+    {
+        id: 36,
+        name: 'Bomet',
+    },
+    {
+        id: 37,
+        name: 'Kakamega',
+    },
+    {
+        id: 38,
+        name: 'Vihiga',
+    },
+    {
+        id: 39,
+        name: 'Bungoma',
+    },
+    {
+        id: 40,
+        name: 'Busia',
+    },
+    {
+        id: 41,
+        name: 'Siaya',
+    },
+    {
+        id: 42,
+        name: 'Kisumu',
+    },
+    {
+        id: 43,
+        name: 'Homa Bay',
+    },
+    {
+        id: 44,
+        name: 'Migori',
+    },
+    {
+        id: 45,
+        name: 'Kisii',
+    },
+    {
+        id: 46,
+        name: 'Nyamira',
+    },
+    {
+        id: 47,
+        name: 'Nairobi',
+        subLocations: [
+            {
+                id: 1,
+                name: 'Kahawa',
+            },
+            {
+                id: 2,
+                name: 'Roysambu',
+            },
+            {
+                id: 3,
+                name: 'Githurai',
+            },
+            {
+                id: 4,
+                name: 'zimmerman',
+            },
+            {
+                id: 5,
+                name: 'Kasarani',
+            },
+            {
+                id: 6,
+                name: 'Embakasi',
+            },
+            {
+                id: 7,
+                name: 'Dagoretti',
+            },
+            {
+                id: 6,
+                name: 'westlands',
+            },
+            {
+                id: 8,
+                name: 'makadara',
+            },
+            {
+                id: 9,
+                name: 'Eastleigh',
+            },
+            {
+                id: 10,
+                name: 'starehe',
+            },
+            {
+                id: 11,
+                name: 'mathare',
+            },
+        ],
+    },
 ];
