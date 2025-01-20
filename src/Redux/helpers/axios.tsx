ee57885714ca9c553f2ce2d77f24c0d9e182bf4b
Jane49-cloud <janendirangu49@gmail.com>
2025-01-20 16:11:08 +0300

    update baseurl

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosService = axios.create({
-    baseURL: 'https://api.eduka.co.ke',
-    // baseURL: 'http://localhost:8000',
-    // baseURL: 'https://137.184.189.199:8000',
+  baseURL: "https://ecommerce.eleliafrika.cloud",
+  // baseURL: 'http://localhost:8000',
+  // baseURL: 'https://137.184.189.199:8000',
 });
 
 axiosService.interceptors.request.use(async (req) => {
-    let token = localStorage.getItem('userToken');
-    if (token) {
-        req.headers['x-access-token'] = token;
-    }
-    return req;
+  let token = localStorage.getItem("userToken");
+  if (token) {
+    req.headers["x-access-token"] = token;
+  }
+  return req;
 });
